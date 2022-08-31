e4958bc7864bf1257f75720c199f2f3d0562a505
lgordillo <lgordillo@vates.com>
2022-08-30 21:48:56 -0300

    Refactor de Streamer -> CreateStreamer Implementación de Validator en CreateStreamer

---
+++ CleanArchitecture.Application/Features/Streamers/Commands/CreateStreamer/CreateStreamerCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace CleanArchitecture.Application.Features.Streamers.Commands.CreateStreamer
+{
+    public class CreateStreamerCommandValidator: AbstractValidator<CreateStreamerCommand>
+    {
+        public CreateStreamerCommandValidator()
+        {
+            RuleFor(static p => p.Nombre)
+                .NotEmpty().WithMessage("{Nombre} no puede estar en blanco")
+                .NotNull().WithMessage("{Nombre} no puede estar en blanco")
+                .MaximumLength(50).WithMessage("{Nombre} no puede superar los 50 caracteres");
+
+            RuleFor(static p => p.Url)
+                .NotEmpty().WithMessage("{Url} no puede estar en blanco");
+        }
+    }
+}
