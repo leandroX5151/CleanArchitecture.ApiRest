ef2793859ab9359d11579d26f72d43c65f2175ce
lgordillo <lgordillo@vates.com>
2022-08-31 17:18:58 -0300

    Implementación de MadiatR y Behaviors

---
+++ CleanArchitecture.Application/Behaviors/UnhandlerExceptionBehavior.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Application.Behaviors
+{
+    public class UnhandlerExceptionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : Exception, IRequest<TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandlerExceptionBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            // Solo analizar lós métodos Handlers de los Querys y Commands
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex,
+                    $"Application Request: Sucedió una excepción para el request {requestName} {request}");
+                throw;
+            }
+        }
+    }
+}
---
+++ CleanArchitecture.Application/Behaviors/ValidationBehaviors.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using MediatR;
+using ValidationException = CleanArchitecture.Application.Exceptions.ValidationException;
+
+namespace CleanArchitecture.Application.Behaviors
+{
+    public class ValidationBehaviors<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviors(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                // Este método ejecuta todas las validaciones definidas en las clases 'xxCommandValidator'
+                // antes de que lleguen a las Features
+                var validationResults =
+                    await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+                var failures = validationResults.SelectMany(static r => r.Errors).Where(static f => f != null).ToList();
+
+                if (failures.Count > 0)
+                {
+                    throw new ValidationException(failures);
+                }
+            }
+
+            return await next();
+        }
+    }
+}
