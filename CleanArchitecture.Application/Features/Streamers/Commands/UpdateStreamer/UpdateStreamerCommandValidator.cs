1dd1ef5f21d9d9f25bd9563ef2f3cef5ecf5444b
lgordillo <lgordillo@vates.com>
2022-08-30 22:16:46 -0300

    Implementación de UpdateStreamer

---
+++ CleanArchitecture.Application/Features/Streamers/Commands/UpdateStreamer/UpdateStreamerCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace CleanArchitecture.Application.Features.Streamers.Commands.UpdateStreamer
+{
+    public  class UpdateStreamerCommandValidator: AbstractValidator<UpdateStreamerCommand>
+    {
+        public UpdateStreamerCommandValidator()
+        {
+            RuleFor(static p => p.Name)
+                .NotNull().WithMessage("{Nombre} no s epermiten nulos");
+            RuleFor(static p => p.Url)
+                .NotNull().WithMessage("{Url} no s epermiten nulos");
+        }
+    }
+}
